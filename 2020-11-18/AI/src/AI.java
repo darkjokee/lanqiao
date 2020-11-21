c0234debcc4c2f9da09fb17ca55a592a496f1d0b
darkjokee <735391949@qq.com>
2020-11-21 10:37:16 +0800

    二维数组的使用

--- 2020-11-18/AI/src/AI.java
+++ 2020-11-18/AI/src/AI.java
@@ -27,7 +27,7 @@ class MainCanvas extends Canvas
 		try
 		{
 		for(i=0;i<3;i++)
-			for(j=0;j<4;j++){
+			for(j=0;j<7;j+=2){
 				heroImg[i][j]=Image.createImage("/sayo"+i+""+j+".png");
 			}
 			currentImg=heroImg[1][0];
@@ -39,18 +39,17 @@ class MainCanvas extends Canvas
 		}
 		x=120;
 		y=100;
-
 	}
 	public void keyReleased(int keyCode){
 		int action=getGameAction(keyCode);
 		if(action==LEFT){
-			currentImg=heroImg[1][1];
+			currentImg=heroImg[1][2];
 		}
 		if(action==RIGHT){
-			currentImg=heroImg[1][3];
+			currentImg=heroImg[1][6];
 		}
 		if(action==UP){
-			currentImg=heroImg[1][2];
+			currentImg=heroImg[1][4];
 		}
 		if(action==DOWN){
 			currentImg=heroImg[1][0];
@@ -62,33 +61,32 @@ class MainCanvas extends Canvas
 		g.fillRect(0,0,getWidth(),getHeight());
 		g.drawImage(currentImg,x,y,0);
 	}
-
 	public void keyPressed(int keyCode){
 		int action=getGameAction(keyCode);
 		if(action==LEFT){
 			x=x-5;
 			if(x%2==0)
-				currentImg=heroImg[0][1];
-			else
-				currentImg=heroImg[2][1];
-		}
-
-		if(action==UP){
-			y=y-9;
-			if(y%2==0)
 				currentImg=heroImg[0][2];
 			else
 				currentImg=heroImg[2][2];
 		}
+
+		if(action==UP){
+			y=y-7;
+			if(y%2==0)
+				currentImg=heroImg[0][4];
+			else
+				currentImg=heroImg[2][4];
+		}
 		if(action==RIGHT){
 			x=x+5;
 			if(x%2==0)
-				currentImg=heroImg[0][3];
+				currentImg=heroImg[0][6];
 			else
-				currentImg=heroImg[2][3];
+				currentImg=heroImg[2][6];
 		}
 		if(action==DOWN){
-			y=y+9;
+			y=y+7;
 			if(y%2==0)
 				currentImg=heroImg[0][0];
 			else
